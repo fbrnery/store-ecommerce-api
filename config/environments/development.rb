bdbf66e7f53ef06a5cb61ac31433ccba8af690e8
Fabricio Miranda <fbrnery@gmail.com>
2023-05-14 22:44:39 -0300

    Configuring email delivery

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,18 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+   config.active_storage.service = :local
+
+   config.action_mailer.raise_delivery_errors = false
+
+   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+
+   config.action_mailer.delivery_method = :smtp
+
+   config.action_mailer.smtp_settings = {
+    address: 'localhost',
+    port: 1025,
+}
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
